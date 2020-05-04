8539ce9a64a646065ff90681ccb3b6d5533dd97a
lilpolymath <ayofavour7@gmail.com>
2020-05-04 10:29:35 +0100

    Updates

---
+++ precache-manifest.251343755ff00b3bac71e28635419a79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f54b5ddbb0835ebb7c4a5edabf1b199",
+    "url": "/landing-pages/index.html"
+  },
+  {
+    "revision": "a1e63ad1617a73c8703e",
+    "url": "/landing-pages/static/css/main.ef880803.chunk.css"
+  },
+  {
+    "revision": "645dd4716cb45601ab4d",
+    "url": "/landing-pages/static/js/2.014d7530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-pages/static/js/2.014d7530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e63ad1617a73c8703e",
+    "url": "/landing-pages/static/js/main.06a22e3d.chunk.js"
+  },
+  {
+    "revision": "25508c898417aaebd049",
+    "url": "/landing-pages/static/js/runtime-main.1704dae0.js"
+  },
+  {
+    "revision": "b7059fd47082c33b56f1ea209dcb095f",
+    "url": "/landing-pages/static/media/Placeholder.b7059fd4.png"
+  }
+]);
